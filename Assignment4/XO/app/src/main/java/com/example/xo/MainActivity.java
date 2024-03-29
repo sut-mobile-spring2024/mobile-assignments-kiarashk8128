6986c0b97b750ace86164789b1b8e900b39c7327
Kian <oxfvard@gmail.com>
2024-03-29 23:51:50 +0330

    XO game

---
+++ Assignment4/XO/app/src/main/java/com/example/xo/MainActivity.java
@@ -0,0 +1,124 @@
+package com.example.xo;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button[][] buttons = new Button[3][3];
+    private boolean player1Turn = true;
+    private int roundCount;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Initialize buttons
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                String buttonID = "button" + (i * 3 + j + 1);
+                int resID = getResources().getIdentifier(buttonID, "id", getPackageName());
+                buttons[i][j] = findViewById(resID);
+                buttons[i][j].setOnClickListener(this);
+            }
+        }
+
+        Button buttonClear = findViewById(R.id.clearButton);
+        buttonClear.setOnClickListener(v -> clearBoard());
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (!((Button) v).getText().toString().equals("")) {
+            return;
+        }
+
+        if (player1Turn) {
+            ((Button) v).setText("X");
+            ((Button) v).setTextColor(getResources().getColor(R.color.pink));
+        } else {
+            ((Button) v).setText("O");
+            ((Button) v).setTextColor(getResources().getColor(R.color.yellow));
+        }
+
+        roundCount++;
+
+        if (checkForWin()) {
+            if (player1Turn) {
+                player1Wins();
+            } else {
+                player2Wins();
+            }
+        } else if (roundCount == 9) {
+            draw();
+        } else {
+            player1Turn = !player1Turn;
+        }
+    }
+
+    private boolean checkForWin() {
+        String[][] field = new String[3][3];
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                field[i][j] = buttons[i][j].getText().toString();
+            }
+        }
+
+        for (int i = 0; i < 3; i++) {
+            if (field[i][0].equals(field[i][1])
+                    && field[i][0].equals(field[i][2])
+                    && !field[i][0].equals("")) {
+                return true;
+            }
+            if (field[0][i].equals(field[1][i])
+                    && field[0][i].equals(field[2][i])
+                    && !field[0][i].equals("")) {
+                return true;
+            }
+        }
+
+        if (field[0][0].equals(field[1][1])
+                && field[0][0].equals(field[2][2])
+                && !field[0][0].equals("")) {
+            return true;
+        }
+        if (field[0][2].equals(field[1][1])
+                && field[0][2].equals(field[2][0])
+                && !field[0][2].equals("")) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private void player1Wins() {
+        Toast.makeText(this, "Player X wins!", Toast.LENGTH_LONG).show();
+        clearBoard();
+    }
+
+    private void player2Wins() {
+        Toast.makeText(this, "Player O wins!", Toast.LENGTH_LONG).show();
+        clearBoard();
+    }
+
+    private void draw() {
+        Toast.makeText(this, "Draw!", Toast.LENGTH_LONG).show();
+        clearBoard();
+    }
+
+    private void clearBoard() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                buttons[i][j].setText("");
+            }
+        }
+
+        roundCount = 0;
+        player1Turn = true;
+    }
+}
